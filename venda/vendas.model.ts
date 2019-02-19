8948084b3b7e9de49588961d5a39df3cfa3ae9b4
Fábio <viana1505@gmail.com>
2019-02-19 00:53:18 -0300

    Mudança Model e Router de Vendas

--- venda/vendas.model.ts
+++ venda/vendas.model.ts
@@ -2,14 +2,25 @@ import * as mongoose from 'mongoose'
 import {Empresas} from '../empresa/empresa.model'
 import {Produtos} from '../produto/produto.model'
 
+export interface listaProdutoSchema extends mongoose.Document{
+  produto: mongoose.Types.ObjectId | Produtos
+}
 
 export interface Vendas extends mongoose.Document {
   valor_total: number,
   valor_desconto: number,
   nome_cli: string,
-  produto: mongoose.Types.ObjectId | Produtos,
+  produtos: listaProdutoSchema[],
   empresa: mongoose.Types.ObjectId | Empresas,
 }
+
+const listaProdutoSchema = new mongoose.Schema({
+  produto: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: false,
+    ref: 'Produtos',
+    default:[]
+  }})
 
 const vendaSchema = new mongoose.Schema({
   valor_total: {
@@ -25,10 +36,10 @@ const vendaSchema = new mongoose.Schema({
     required: true,
     maxlength: 100
   },
-  produto: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'Produtos'
+  produtos: {
+      type: [listaProdutoSchema],
+      required: false,
+      default:[]
   },
   empresa: {
     type: mongoose.Schema.Types.ObjectId,
--- venda/vendas.router.ts
+++ venda/vendas.router.ts
@@ -1,6 +1,7 @@
 import * as restify from 'restify'
 import * as mongoose from 'mongoose'
 import {ModelRouter} from '../common/model-router'
+import {NotFoundError} from 'restify-errors'
 import {Vendas} from './vendas.model'
 
 class VendasRouter extends ModelRouter<Vendas>{
@@ -8,10 +9,41 @@ class VendasRouter extends ModelRouter<Vendas>{
     super(Vendas)
   }
 
+  findProdutos = (req, resp, next) => {
+    Vendas.findById(req.params.id, "+produtos")
+       .then(vendas =>{
+         if(!vendas){
+           throw new NotFoundError('Vendas not found')
+         }else{
+           resp.json(vendas.produtos)
+           return next()
+         }
+       }).catch(next)
+  }
+
+  adicionaProdutos = (req, resp, next)=>{
+      Vendas.findById(req.params.id, "+produtos").then(vendas=>{
+        if(!vendas){
+          throw new NotFoundError('Vendas not found')
+        }else{
+          console.log(`${vendas.produtos}`)
+          //vendas.produtos = req.body
+          vendas.produtos.push(req.body)
+          console.log(`${vendas.produtos}`)
+          vendas.save()
+              .then(this.render(resp, next))
+              .catch(next)
+            }
+})}
+
   applyRoutes(application: restify.Server){
     application.get('/vendas', this.findAll)
     application.get('/vendas/:id', [this.validateId ,this.findById])
     application.post('/vendas', this.save)
+
+    application.get('/vendas/:id/produtos', this.findProdutos)
+    application.put('/vendas/:id/produtosf', this.adicionaProdutos)
+
   }
 }
 
